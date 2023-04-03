9c8554885b71707a1880a0490cfc0191b8cec94a
Quyson Nguyen <Quyson.nguyen.1999@gmail.com>
2023-04-03 11:24:49 -0700

    Create single dynamic user page

--- client/src/App.js
+++ client/src/App.js
@@ -4,6 +4,7 @@ import Home from "./Home";
 import Login from "./Login";
 import Signup from "./Signup";
 import Users from './Users';
+import User from "./User";
 
 
 function App() {
@@ -14,6 +15,7 @@ function App() {
         <Route path="/login" element={<Login />}></Route>
         <Route path="/signup" element={<Signup />}></Route>
         <Route path="/users" element={<Users />}></Route>
+        <Route path ="/users/:id" element={<User />}></Route>
       </Routes>
     </BrowserRouter>
   );
---
+++ client/src/User.js
@@ -0,0 +1,50 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import { useParams } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+const User = () => {
+
+    const { id } = useParams();
+    const [user, setUser] = useState(null);
+    const [posts, setPosts] = useState(null);
+    const [currentUser, setCurrentUser] = useState("");
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        axios.get(`http://localhost:8000/users/${id}`, { headers: {Authorization : token}}).then((response) => {
+            setUser(response.data.resultUser);
+            setPosts(response.data.resultPost);
+            setCurrentUser(response.data.currentUser);
+        });
+    }, [])
+
+    console.log(user);
+    return(
+        <div>
+            <div>
+                { currentUser ? <div>{currentUser.username}</div> : null}
+            </div>
+           <div>
+                { user ?  
+                <div>
+                    <h1>{user.username}</h1>
+                    <div>{user.first_name + " " + user.last_name}</div>
+                    <div>{user.bio}</div>
+                </div> : null}
+           </div>
+           <h1>Posts</h1>
+           { posts && posts.length > 0 ? posts.map((element) => {
+                return(
+                    <div>
+                        <Link to={`/posts/${element._id}`}><h6>{element.title}</h6></Link>
+                        <p>{element.message}</p>
+                        <p>{element.likes}</p>
+                    </div>
+                )
+            }) : <div>No Posts to Show...</div>}
+        </div>
+    );
+};
+
+export default User
